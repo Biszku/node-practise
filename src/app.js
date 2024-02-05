99bf505d2862e29bdea09432f87407e10f9ed460
Biszku <dbichu@onet.pl>
2024-02-05 14:36:33 +0100

    Handle bar practise

--- src/app.js
+++ src/app.js
@@ -7,6 +7,27 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Daniel Bichuniak",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Daniel Bichuniak",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This  is some helpful text.",
+    name: "Daniel Bichuniak",
+  });
+});
+
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "It is snowing",
